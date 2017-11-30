42e42c5e6bf132560980a02563236e636cb85227
Hongbo Zhang <hongbozhang.ca@gmail.com>
2017-11-30 00:22:09 -0500

    periodical update

--- config.py
+++ config.py
@@ -2,12 +2,13 @@ from app import models
 import time
 
 def update_all_audio_url(db):
-    collection = db['pyusic']['youtube']
-    music_info_list = collection.find()
-    musics = [models.YoutubeAudio(info) for info in music_info_list]
-    for music in musics:
-        if music.update_audio_url():
-            music.update(collection)
-            time.sleep(1)
-    print('All audio url updated')
-    time.sleep(3600)
+    while True:
+        collection = db['pyusic']['youtube']
+        music_info_list = collection.find()
+        musics = [models.YoutubeAudio(info) for info in music_info_list]
+        for music in musics:
+            if music.update_audio_url():
+                music.update(collection)
+                time.sleep(1)
+        print('All audio url updated')
+        time.sleep(3600)
